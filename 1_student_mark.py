8c76992b39623ff8e37d727842bb712c60409177
HaiDreamer <nghuyhai2004@gmail.com>
2024-12-18 21:27:34 +0700

    fixxing

--- 1_student_mark.py
+++ 1_student_mark.py
@@ -27,7 +27,7 @@ with open(file_name, "w") as file:
             file.write(f"Course ID: {course_id}, Mark: {course_mark}\n")
 
 #use dict here, extract information into a dict
-#file_name = "students_info.txt"
+file_name = "students_info.txt"
 
 def parse_file(file_name):
     students = {}
@@ -38,8 +38,8 @@ def parse_file(file_name):
             line = line.strip() #remove leading/trailing whitespace
             #check for student id line
             if line.startswith("Student ID"):
-                parts = line.split(", ")
-                student_id = parts[0].split(": ")[1]
+                parts = line.split(", ")    #Splits the string into a list of parts wherever there is a comma followed by a space (, ).
+                student_id = parts[0].split(": ")[1]    #lay phan ben phai cua Student ID(index: 1)
                 name = parts[1].split(": ")[1]
                 dob = parts[2].split(": ")[1]
                 #create new student entry
@@ -59,3 +59,24 @@ def parse_file(file_name):
 
 data = parse_file(file_name)
 print(data)
+
+def display_courses(data):
+    print("\nCourses:")
+    courses_ids = set()
+    for std_id, std_info in data.items:
+        courses = std_info.get('Courses', {})
+        courses_ids.update(courses.keys())
+    courses_ids = list(courses_ids)     #convert it into list
+    print(courses_ids)
+
+def display_students(data):
+    print("\nStudents: ")
+    students_names = set()
+    for std_id, std_info in data.items:
+        names = std_info.get('Name', {})
+        students_names.update(names.keys())
+    students_names = list(students_names)
+    print(students_names)
+
+display_courses(data)
+display_students(data)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,3 @@
-#use dict here, extract information into a dict
 file_name = "students_info.txt"
 
 def parse_file(file_name):
@@ -10,8 +9,8 @@ def parse_file(file_name):
             line = line.strip() #remove leading/trailing whitespace
             #check for student id line
             if line.startswith("Student ID"):
-                parts = line.split(", ")
-                student_id = parts[0].split(": ")[1]
+                parts = line.split(", ")    #Splits the string into a list of parts wherever there is a comma followed by a space (, ).
+                student_id = parts[0].split(": ")[1]    #lay phan ben phai cua Student ID(index: 1)
                 name = parts[1].split(": ")[1]
                 dob = parts[2].split(": ")[1]
                 #create new student entry
@@ -29,5 +28,23 @@ def parse_file(file_name):
                     students[current_student]["Courses"][course_id] = mark
     return students
 
-data  = parse_file(file_name)
+data = parse_file(file_name)
 print(data)
+
+def display_courses(data):
+    print("\nCourses:")
+    courses_ids = set()
+    for std_id, std_info in data.items:
+        courses = std_info.get('Courses', {})
+        courses_ids.update(courses.keys())
+    courses_ids = list(courses_ids)     #convert it into list
+    print(courses_ids)
+
+def display_students(data):
+    print("\nStudents: ")
+    students_names = set()
+    for std_id, std_info in data.items:
+        names = std_info.get('Name', {})
+        students_names.update(names.keys())
+    students_names = list(students_names)
+    print(students_names)
